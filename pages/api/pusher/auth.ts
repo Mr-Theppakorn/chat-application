ac9812b59f79b2c225d1cd2418e2e0eec7b08309
Mr-Theppakorn <38773611+Mr-Theppakorn@users.noreply.github.com>
2024-04-04 19:36:58 +0700

    Update auth.ts

--- pages/api/pusher/auth.ts
+++ pages/api/pusher/auth.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import authOptions from "../../../libs/auth";
 import { pusherServer } from "@/libs/pusher";
 
 export default async function handler(
